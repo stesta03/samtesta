89a4df7af6251e277f4c347832c3634fcc7173b5
stesta03 <samantha.testa@tufts.edu>
2019-02-22 00:49:37 -0500

    got lab4 files into responsive folder, had to reclone git repo and began javascript for lab5 but having some trouble :(

---
+++ butwehavethepower/subtitles.js
@@ -0,0 +1,9 @@
+
+
+    var vid = document.getElementById("butwehavethepower");
+    vid.addEventListener("timeupdate", function)
+    if(vid.currentTime >= 0 and vid.currentTime <= 15){
+
+
+    }
+}
